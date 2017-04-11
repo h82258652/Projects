06ed5895de8d5a05f33f4aa180b48870fa53b38b
h82258652 <h82258652@hotmail.com>
2017-04-11 14:28:58 +0800

    update

---
+++ Libraries/SoftwareKobo.Utils.Uwp/Extensions/RectExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Windows.Foundation;
+
+namespace SoftwareKobo.Extensions
+{
+    public static class RectExtensions
+    {
+        public static bool IntersectsWith(this Rect rect1, Rect rect2)
+        {
+            if (rect1.IsEmpty || rect2.IsEmpty)
+            {
+                return false;
+            }
+
+            return rect1.Left <= rect2.Right
+                   && rect1.Right >= rect2.Left
+                   && rect1.Top <= rect2.Bottom
+                   && rect1.Bottom >= rect2.Top;
+        }
+    }
+}
