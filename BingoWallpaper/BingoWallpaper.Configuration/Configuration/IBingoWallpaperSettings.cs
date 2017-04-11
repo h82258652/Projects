edc6784a826ad27d85d439adbafc54ef6d015862
h82258652 <h82258652@hotmail.com>
2017-04-11 11:07:08 +0800

    update configuration

---
+++ BingoWallpaper/BingoWallpaper.Configuration/Configuration/IBingoWallpaperSettings.cs
@@ -0,0 +1,37 @@
+﻿using BingoWallpaper.Models;
+
+namespace BingoWallpaper.Configuration
+{
+    public interface IBingoWallpaperSettings
+    {
+        bool IsAutoUpdateLockScreen
+        {
+            get;
+            set;
+        }
+
+        bool IsAutoUpdateWallpaper
+        {
+            get;
+            set;
+        }
+
+        string SelectedArea
+        {
+            get;
+            set;
+        }
+
+        SaveLocation SelectedSaveLocation
+        {
+            get;
+            set;
+        }
+
+        WallpaperSize SelectedWallpaperSize
+        {
+            get;
+            set;
+        }
+    }
+}
