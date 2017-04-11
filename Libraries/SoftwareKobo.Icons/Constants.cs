8934d518f105368a18bcd11e030d381296e894c4
h82258652 <h82258652@hotmail.com>
2017-04-11 15:31:21 +0800

    add icons

---
+++ Libraries/SoftwareKobo.Icons/Constants.cs
@@ -0,0 +1,13 @@
+﻿namespace SoftwareKobo.Icons
+{
+    internal static class Constants
+    {
+        internal const string DashiconsFamilyName = "ms-appx:///SoftwareKobo.Icons/Assets/Fonts/dashicons_3.8.ttf#Dashicons";
+
+        internal const string FontAwesomeFamilyName = "ms-appx:///SoftwareKobo.Icons/Assets/Fonts/FontAwesome_4.7.0.otf#FontAwesome";
+
+        internal const string IoniconsFamilyName = "ms-appx:///SoftwareKobo.Icons/Assets/Fonts/ionicons_2.1.0.ttf#Ionicons";
+
+        internal const string MaterialIconsFamilyName = "ms-appx:///SoftwareKobo.Icons/Assets/Fonts/MaterialIcons-Regular_2.2.0.ttf#Material Icons";
+    }
+}
