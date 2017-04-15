6ad7132110ea58c0ddb66bc2e2992a4454aae2d3
h82258652 <h82258652@hotmail.com>
2017-04-15 22:37:53 +0800

    update

--- U148/U148.Uwp.14393/Controls/ArticleItem.xaml.cs
+++ U148/U148.Uwp.14393/Controls/ArticleItem.xaml.cs
@@ -1,10 +1,24 @@
-﻿namespace U148.Uwp.Controls
+﻿using System.Diagnostics;
+using U148.Models;
+
+namespace U148.Uwp.Controls
 {
     public sealed partial class ArticleItem
     {
         public ArticleItem()
         {
             InitializeComponent();
+
+            this.DataContextChanged += ArticleItem_DataContextChanged;
+        }
+
+        private void ArticleItem_DataContextChanged(Windows.UI.Xaml.FrameworkElement sender, Windows.UI.Xaml.DataContextChangedEventArgs args)
+        {
+            var article = args.NewValue as Article;
+            var p = article?.PicMid;
+            if (string.IsNullOrEmpty(p))
+            {
+            }
         }
     }
 }
---
+++ U148/U148.Uwp.14393/Converters/IsNullOrEmptyStringToVisibilityConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace U148.Uwp.Converters
+{
+    public class IsNullOrEmptyStringToVisibilityConverter : IValueConverter
+    {
+        public bool IsInversed
+        {
+            get;
+            set;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (IsInversed)
+            {
+                return string.IsNullOrEmpty(value as string) ? Visibility.Visible : Visibility.Collapsed;
+            }
+            else
+            {
+                return string.IsNullOrEmpty(value as string) ? Visibility.Collapsed : Visibility.Visible;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
