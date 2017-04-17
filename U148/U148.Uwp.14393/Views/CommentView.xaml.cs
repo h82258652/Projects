a7398ac46ce22cebd169984d1da262836be95155
h82258652 <h82258652@hotmail.com>
2017-04-17 16:57:44 +0800

    update

--- U148/U148.Uwp.14393/Views/CommentView.xaml.cs
+++ U148/U148.Uwp.14393/Views/CommentView.xaml.cs
@@ -1,10 +1,31 @@
-﻿namespace U148.Uwp.Views
+﻿using Windows.UI.Xaml.Navigation;
+using GalaSoft.MvvmLight.Messaging;
+using U148.Uwp.Messages;
+
+namespace U148.Uwp.Views
 {
     public sealed partial class CommentView
     {
         public CommentView()
         {
             InitializeComponent();
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+
+            Messenger.Default.Unregister(this);
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            Messenger.Default.Register<SendCommentSuccessMessage>(this, message =>
+            {
+                CommentTextBox.Text = string.Empty;
+            });
         }
     }
 }
