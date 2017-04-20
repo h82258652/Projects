9db64d2774265dadb335d6ddc354cae5a6fc3777
h82258652 <h82258652@hotmail.com>
2017-04-20 15:01:38 +0800

    update

--- U148/U148.Uwp.14393/ViewModels/AboutViewModel.cs
+++ U148/U148.Uwp.14393/ViewModels/AboutViewModel.cs
@@ -1,30 +1,8 @@
 ﻿using GalaSoft.MvvmLight;
-using GalaSoft.MvvmLight.Command;
-using SoftwareKobo.Services;
 
 namespace U148.Uwp.ViewModels
 {
     public class AboutViewModel : ViewModelBase
     {
-        private readonly IStoreService _storeService;
-
-        private RelayCommand _praiseCommand;
-
-        public AboutViewModel(IStoreService storeService)
-        {
-            _storeService = storeService;
-        }
-
-        public RelayCommand PraiseCommand
-        {
-            get
-            {
-                _praiseCommand = _praiseCommand ?? new RelayCommand(async () =>
-                {
-                    await _storeService.OpenCurrentAppReviewPageAsync();
-                });
-                return _praiseCommand;
-            }
-        }
     }
 }
