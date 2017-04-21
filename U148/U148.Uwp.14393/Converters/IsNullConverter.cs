7ac8cadb466744c018b72775e2e6bd948e6fe00b
h82258652 <h82258652@hotmail.com>
2017-04-21 12:59:44 +0800

    update

---
+++ U148/U148.Uwp.14393/Converters/IsNullConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace U148.Uwp.Converters
+{
+    public class IsNullConverter : IValueConverter
+    {
+        public bool IsInversed
+        {
+            get;
+            set;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (IsInversed)
+            {
+                return value != null;
+            }
+            else
+            {
+                return value == null;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
