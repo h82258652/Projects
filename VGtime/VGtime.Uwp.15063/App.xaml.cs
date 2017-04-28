c798e6b219a8d5e7db6b36cad9475196b552650e
h82258652 <h82258652@hotmail.com>
2017-04-28 16:16:38 +0800

    update

--- VGtime/VGtime.Uwp.15063/App.xaml.cs
+++ VGtime/VGtime.Uwp.15063/App.xaml.cs
@@ -3,12 +3,11 @@ using VGtime.Uwp.Views;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
 namespace VGtime.Uwp
 {
-    public sealed partial class App : Application
+    public sealed partial class App
     {
         public App()
         {
@@ -17,60 +16,33 @@ namespace VGtime.Uwp
             Suspending += OnSuspending;
         }
 
-        private void OnResuming(object sender, object e)
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        /// 在应用程序由最终用户正常启动时进行调用。
-        /// 将在启动应用程序以打开特定文件等情况下使用。
-        /// </summary>
-        /// <param name="e">有关启动请求和过程的详细信息。</param>
         protected override void OnLaunched(LaunchActivatedEventArgs e)
         {
-            Frame rootFrame = Window.Current.Content as Frame;
-
-            // 不要在窗口已包含内容时重复应用程序初始化，
-            // 只需确保窗口处于活动状态
-            if (rootFrame == null)
+            var rootView = Window.Current.Content as RootView;
+            if (rootView == null)
             {
-                // 创建要充当导航上下文的框架，并导航到第一页
-                rootFrame = new Frame();
-
-                rootFrame.NavigationFailed += OnNavigationFailed;
-
-                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
-                {
-                    //TODO: 从之前挂起的应用程序加载状态
-                }
-
-                // 将框架放在当前窗口中
-                Window.Current.Content = rootFrame;
+                rootView = new RootView();
+                rootView.RootFrame.NavigationFailed += OnNavigationFailed;
+                Window.Current.Content = rootView;
             }
 
             if (e.PrelaunchActivated == false)
             {
-                if (rootFrame.Content == null)
+                if (rootView.RootFrame.Content == null)
                 {
-                    // 当导航堆栈尚未还原时，导航到第一页，
-                    // 并通过将所需信息作为导航参数传入来配置
-                    // 参数
-                    rootFrame.Navigate(typeof(MainView), e.Arguments);
+                    rootView.RootFrame.Navigate(typeof(MainView), e.Arguments);
                 }
-                // 确保当前窗口处于活动状态
                 Window.Current.Activate();
             }
         }
 
-        /// <summary>
-        /// 导航到特定页失败时调用
-        /// </summary>
-        ///<param name="sender">导航失败的框架</param>
-        ///<param name="e">有关导航失败的详细信息</param>
         private void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
         {
             throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+        private void OnResuming(object sender, object e)
+        {
         }
 
         private void OnSuspending(object sender, SuspendingEventArgs e)
