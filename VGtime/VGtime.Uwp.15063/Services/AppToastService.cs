f6f918aeab5aef2247ea07a5445479adaa68f554
h82258652 <h82258652@hotmail.com>
2017-05-05 09:58:06 +0800

    update

--- VGtime/VGtime.Uwp.15063/Services/AppToastService.cs
+++ VGtime/VGtime.Uwp.15063/Services/AppToastService.cs
@@ -23,7 +23,7 @@ namespace VGtime.Uwp.Services
         public async void ShowError(string message)
         {
             var toastPrompt = CreateToastPrompt(message);
-            toastPrompt.Background = new SolidColorBrush(Color.FromArgb(255, 234, 23, 32));
+            toastPrompt.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0xEA, 0x17, 0x20));
             toastPrompt.Icon = new FontAwesomeIcon(FontAwesomeSymbol.Close);
             ToastPromptContainer.Children.Add(toastPrompt);
             await toastPrompt.ShowAsync();
@@ -33,7 +33,7 @@ namespace VGtime.Uwp.Services
         public async void ShowInformation(string message)
         {
             var toastPrompt = CreateToastPrompt(message);
-            toastPrompt.Background = new SolidColorBrush(Color.FromArgb(255, 0, 156, 243));
+            toastPrompt.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0, 0x9C, 0xF3));
             toastPrompt.Icon = new FontAwesomeIcon(FontAwesomeSymbol.InfoCircle);
             ToastPromptContainer.Children.Add(toastPrompt);
             await toastPrompt.ShowAsync();
@@ -43,7 +43,7 @@ namespace VGtime.Uwp.Services
         public async void ShowMessage(string message)
         {
             var toastPrompt = CreateToastPrompt(message);
-            toastPrompt.Background = new SolidColorBrush(Color.FromArgb(255, 19, 192, 77));
+            toastPrompt.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0x13, 0xC0, 0x4D));
             toastPrompt.Icon = new FontAwesomeIcon(FontAwesomeSymbol.Check);
             ToastPromptContainer.Children.Add(toastPrompt);
             await toastPrompt.ShowAsync();
@@ -53,7 +53,7 @@ namespace VGtime.Uwp.Services
         public async void ShowWarning(string message)
         {
             var toastPrompt = CreateToastPrompt(message);
-            toastPrompt.Background = new SolidColorBrush(Color.FromArgb(255, 255, 193, 0));
+            toastPrompt.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0xFF, 0xC1, 0));
             toastPrompt.Icon = new FontAwesomeIcon(FontAwesomeSymbol.Warning);
             ToastPromptContainer.Children.Add(toastPrompt);
             await toastPrompt.ShowAsync();
