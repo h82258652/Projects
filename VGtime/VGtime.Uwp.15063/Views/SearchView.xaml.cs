c95a647342eb464e2c34356508ce17799da4830a
h82258652 <h82258652@hotmail.com>
2017-05-05 09:33:44 +0800

    update

--- VGtime/VGtime.Uwp.15063/Views/SearchView.xaml.cs
+++ VGtime/VGtime.Uwp.15063/Views/SearchView.xaml.cs
@@ -1,17 +1,10 @@
-﻿using Windows.UI.Xaml.Controls;
-
-// https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
-
-namespace VGtime.Uwp.Views
+﻿namespace VGtime.Uwp.Views
 {
-    /// <summary>
-    /// 可用于自身或导航至 Frame 内部的空白页。
-    /// </summary>
-    public sealed partial class SearchView : Page
+    public sealed partial class SearchView
     {
         public SearchView()
         {
-            this.InitializeComponent();
+            InitializeComponent();
         }
     }
 }
