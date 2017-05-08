b9780ab0a39827a711656138637b59f2a3f23c5c
h82258652 <h82258652@hotmail.com>
2017-05-08 16:48:44 +0800

    update

--- U148/U148.Uwp.14393/AppThemes/ThemeModeManager.cs
+++ U148/U148.Uwp.14393/AppThemes/ThemeModeManager.cs
@@ -1,17 +1,28 @@
-﻿using GalaSoft.MvvmLight;
+﻿using System;
+using GalaSoft.MvvmLight;
 using U148.Configuration;
 using U148.Models;
+using Weakly;
 
 namespace U148.Uwp.AppThemes
 {
     public class ThemeModeManager : ObservableObject, IThemeModeManager
     {
+        private static readonly WeakEventSource<CurrentThemeChangedEventArgs> EventSource = new WeakEventSource<CurrentThemeChangedEventArgs>();
+
         private readonly IU148UwpSettings _u148UwpSettings;
 
         public ThemeModeManager(IU148UwpSettings u148UwpSettings)
         {
+            EventSource.Add((sender, e) =>
+            {
+                RaisePropertyChanged(() => CurrentTheme);
+            });
+
             _u148UwpSettings = u148UwpSettings;
         }
+
+        public static event EventHandler<CurrentThemeChangedEventArgs> CurrentThemeChanged;
 
         public ThemeMode CurrentTheme
         {
@@ -21,10 +32,18 @@ namespace U148.Uwp.AppThemes
             }
             set
             {
+                if (Enum.IsDefined(typeof(ThemeMode), value) == false)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value));
+                }
+
                 if (_u148UwpSettings.ThemeMode != value)
                 {
                     _u148UwpSettings.ThemeMode = value;
+                    var args = new CurrentThemeChangedEventArgs(value);
+                    EventSource.Raise(this, args);
                     RaisePropertyChanged();
+                    CurrentThemeChanged?.Invoke(this, args);
                 }
             }
         }
