8736b09b28ea12b95098eb2bbbc20e8f5905808a
h82258652 <h82258652@hotmail.com>
2017-05-08 14:54:39 +0800

    update

--- VGtime/VGtime.Uwp.15063/Views/RootView.xaml.cs
+++ VGtime/VGtime.Uwp.15063/Views/RootView.xaml.cs
@@ -1,10 +1,24 @@
-﻿namespace VGtime.Uwp.Views
+﻿using System;
+
+namespace VGtime.Uwp.Views
 {
     public sealed partial class RootView
     {
         public RootView()
         {
             InitializeComponent();
+
+            var splashScreenView = new SplashScreenView();
+            EventHandler initializeCompletedHandler = null;
+            initializeCompletedHandler = async (sender, e) =>
+            {
+                splashScreenView.InitializeCompleted -= initializeCompletedHandler;
+                RootFrame.Navigate(typeof(MainView));
+                await splashScreenView.DismissAsync();
+                RootGrid.Children.Remove(splashScreenView);
+            };
+            splashScreenView.InitializeCompleted += initializeCompletedHandler;
+            RootGrid.Children.Add(splashScreenView);
         }
     }
 }
---
+++ VGtime/VGtime.Uwp.15063/Views/SplashScreenView.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media.Animation;
+using WinRTXamlToolkit.AwaitableUI;
+
+namespace VGtime.Uwp.Views
+{
+    public sealed partial class SplashScreenView
+    {
+        public SplashScreenView()
+        {
+            InitializeComponent();
+        }
+
+        public event EventHandler InitializeCompleted;
+
+        public async Task DismissAsync()
+        {
+            var storyboard = new Storyboard();
+            {
+                var animation = new DoubleAnimation()
+                {
+                    From = 1,
+                    To = 0,
+                    Duration = TimeSpan.FromSeconds(0.4)
+                };
+                Storyboard.SetTarget(animation, RootGrid);
+                Storyboard.SetTargetProperty(animation, "Opacity");
+                storyboard.Children.Add(animation);
+            }
+            {
+                var animation = new DoubleAnimation()
+                {
+                    From = 1,
+                    To = 1.3,
+                    Duration = TimeSpan.FromSeconds(0.4),
+                    EasingFunction = new ExponentialEase()
+                    {
+                        EasingMode = EasingMode.EaseOut,
+                        Exponent = 5
+                    }
+                };
+                Storyboard.SetTarget(animation, RootGrid);
+                Storyboard.SetTargetProperty(animation, "(UIElement.RenderTransform).(ScaleTransform.ScaleX)");
+                storyboard.Children.Add(animation);
+            }
+            {
+                var animation = new DoubleAnimation()
+                {
+                    From = 1,
+                    To = 1.3,
+                    Duration = TimeSpan.FromSeconds(0.4),
+                    EasingFunction = new ExponentialEase()
+                    {
+                        EasingMode = EasingMode.EaseOut,
+                        Exponent = 5
+                    }
+                };
+                Storyboard.SetTarget(animation, RootGrid);
+                Storyboard.SetTargetProperty(animation, "(UIElement.RenderTransform).(ScaleTransform.ScaleY)");
+                storyboard.Children.Add(animation);
+            }
+            await storyboard.BeginAsync();
+        }
+
+        private static void InitializeTitleBar()
+        {
+            var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+            var accentColor = (Color)Application.Current.Resources["VGtimeAccentColor"];
+            titleBar.BackgroundColor = accentColor;
+        }
+
+        private void SplashScreenImage_ImageOpened(object sender, RoutedEventArgs e)
+        {
+            InitializeTitleBar();
+
+            Window.Current.Activate();
+
+            InitializeCompleted?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
