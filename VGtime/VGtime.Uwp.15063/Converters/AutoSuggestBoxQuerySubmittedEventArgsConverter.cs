2d5d98a39d074446ef71603a1e795ba7b2e32c79
h82258652 <h82258652@hotmail.com>
2017-05-09 15:59:30 +0800

    update

---
+++ VGtime/VGtime.Uwp.15063/Converters/AutoSuggestBoxQuerySubmittedEventArgsConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+
+namespace VGtime.Uwp.Converters
+{
+    public class AutoSuggestBoxQuerySubmittedEventArgsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var args = value as AutoSuggestBoxQuerySubmittedEventArgs;
+            return args?.QueryText;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- VGtime/VGtime.Uwp.15063/Views/SearchView.xaml.cs
+++ VGtime/VGtime.Uwp.15063/Views/SearchView.xaml.cs
@@ -1,18 +1,10 @@
-﻿using VGtime.Uwp.ViewModels;
-
-namespace VGtime.Uwp.Views
+﻿namespace VGtime.Uwp.Views
 {
     public sealed partial class SearchView
     {
         public SearchView()
         {
             InitializeComponent();
-        }
-
-        private void AutoSuggestBox_QuerySubmitted(Windows.UI.Xaml.Controls.AutoSuggestBox sender, Windows.UI.Xaml.Controls.AutoSuggestBoxQuerySubmittedEventArgs args)
-        {
-            var vm = DataContext as SearchViewModel;
-            vm.SearchCommand.Execute(args.QueryText);
         }
     }
 }
