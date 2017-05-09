e6a6908c71a5ddf890d1638d633c48e6c9d18c9b
h82258652 <h82258652@hotmail.com>
2017-05-09 09:25:24 +0800

    update

--- U148/U148.Uwp.14393/Assets/Html/js/comment.js
+++ U148/U148.Uwp.14393/Assets/Html/js/comment.js
@@ -2,6 +2,17 @@
 
 function setContent(content) {
     $("#container").html(content);
+    window.external.notify("?action=heightChanged&height=" + document.body.offsetHeight.toString());
+}
+
+function setThemeMode(theme) {
+    theme = theme.toLowerCase();
+    if (theme === "night" || theme === "dark") {
+        document.getElementsByTagName("html")[0].setAttribute("class", "night");
+    }
+    else {
+        document.getElementsByTagName("html")[0].setAttribute("class", "");
+    }
 }
 
 setInterval(function () {
