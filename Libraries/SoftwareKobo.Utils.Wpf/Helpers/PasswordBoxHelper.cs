89725774c20ad0bdbd088ba0fd991aa3dc60369e
h82258652 <h82258652@hotmail.com>
2017-06-20 10:51:17 +0800

    add passwordbox helper

---
+++ Libraries/SoftwareKobo.Utils.Wpf/Helpers/PasswordBoxHelper.cs
@@ -0,0 +1,69 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SoftwareKobo.Helpers
+{
+    public static class PasswordBoxHelper
+    {
+        public static readonly DependencyProperty AttachProperty = DependencyProperty.RegisterAttached("Attach", typeof(bool), typeof(PasswordBoxHelper), new PropertyMetadata(false, OnAttachChanged));
+
+        public static readonly DependencyProperty PasswordProperty = DependencyProperty.RegisterAttached("Password", typeof(string), typeof(PasswordBoxHelper), new PropertyMetadata(string.Empty, OnPasswordChanged));
+
+        private static readonly DependencyProperty IsUpdatingProperty = DependencyProperty.RegisterAttached("IsUpdating", typeof(bool), typeof(PasswordBoxHelper), new PropertyMetadata(false));
+
+        public static string GetPassword(PasswordBox obj)
+        {
+            return (string)obj.GetValue(PasswordProperty);
+        }
+
+        public static void SetPassword(PasswordBox obj, string value)
+        {
+            obj.SetValue(PasswordProperty, value);
+        }
+
+        private static bool GetIsUpdating(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(IsUpdatingProperty);
+        }
+
+        private static void OnAttachChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var obj = (PasswordBox)d;
+
+            if ((bool)e.OldValue)
+            {
+                obj.PasswordChanged -= PasswordChanged;
+            }
+            if ((bool)e.NewValue)
+            {
+                obj.PasswordChanged += PasswordChanged;
+            }
+        }
+
+        private static void OnPasswordChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var obj = (PasswordBox)d;
+            var value = (string)e.NewValue;
+
+            obj.PasswordChanged -= PasswordChanged;
+            if (!GetIsUpdating(obj))
+            {
+                obj.Password = value;
+            }
+            obj.PasswordChanged += PasswordChanged;
+        }
+
+        private static void PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            var passwordBox = (PasswordBox)sender;
+            SetIsUpdating(passwordBox, true);
+            SetPassword(passwordBox, passwordBox.Password);
+            SetIsUpdating(passwordBox, false);
+        }
+
+        private static void SetIsUpdating(DependencyObject obj, bool value)
+        {
+            obj.SetValue(IsUpdatingProperty, value);
+        }
+    }
+}
