dac25e2d9103da8260240d20c72214bdfbc673f8
h82258652 <h82258652@hotmail.com>
2017-06-28 14:18:26 +0800

    update

---
+++ VGtime.V2/VGtime.Uwp.15063/Converters/IntegerBiggerThanOneToVisibilityConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace VGtime.Uwp.Converters
+{
+    public class IntegerBiggerThanOneToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var integer = System.Convert.ToInt64(value);
+            return integer > 1 ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
