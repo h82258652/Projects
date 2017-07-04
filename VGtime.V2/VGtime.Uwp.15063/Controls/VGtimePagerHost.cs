35e4d1f518333b48f014365bc94f6cbbb92640ed
h82258652 <h82258652@hotmail.com>
2017-07-04 18:43:17 +0800

    update

---
+++ VGtime.V2/VGtime.Uwp.15063/Controls/VGtimePagerHost.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace VGtime.Uwp.Controls
+{
+    [TemplateVisualState(GroupName = CommonStateGroupName, Name = "TODO")]
+    [TemplateVisualState(GroupName = CommonStateGroupName, Name = "TODO")]
+    public sealed class VGtimePagerHost : Control
+    {
+        public static readonly DependencyProperty IsVisibleProperty = DependencyProperty.Register(nameof(IsVisible), typeof(bool), typeof(VGtimePagerHost), new PropertyMetadata(default(bool), OnIsVisibleChanged));
+
+        private const string CommonStateGroupName = "CommonStates";
+
+        private const string CollapsedStateName = "Collapsed";
+
+        private const string VisibleStateName = "Visible";
+
+        public VGtimePagerHost()
+        {
+            DefaultStyleKey = typeof(VGtimePagerHost);
+        }
+
+        public bool IsVisible
+        {
+            get
+            {
+                return (bool)GetValue(IsVisibleProperty);
+            }
+            set
+            {
+                SetValue(IsVisibleProperty, value);
+            }
+        }
+
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+        }
+
+        private static void OnIsVisibleChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
