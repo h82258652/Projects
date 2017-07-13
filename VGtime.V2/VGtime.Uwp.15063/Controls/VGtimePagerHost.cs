058ec7278c4f09d1e921d3c5dc4d5f5a7260f8db
h82258652 <h82258652@hotmail.com>
2017-07-13 12:35:48 +0800

    update

--- VGtime.V2/VGtime.Uwp.15063/Controls/VGtimePagerHost.cs
+++ VGtime.V2/VGtime.Uwp.15063/Controls/VGtimePagerHost.cs
@@ -1,19 +1,16 @@
-﻿using System;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace VGtime.Uwp.Controls
 {
-    [TemplateVisualState(GroupName = CommonStateGroupName, Name = "TODO")]
-    [TemplateVisualState(GroupName = CommonStateGroupName, Name = "TODO")]
+    [TemplateVisualState(GroupName = CommonStateGroupName, Name = VisibleStateName)]
+    [TemplateVisualState(GroupName = CommonStateGroupName, Name = CollapsedStateName)]
     public sealed class VGtimePagerHost : Control
     {
         public static readonly DependencyProperty IsVisibleProperty = DependencyProperty.Register(nameof(IsVisible), typeof(bool), typeof(VGtimePagerHost), new PropertyMetadata(default(bool), OnIsVisibleChanged));
 
-        private const string CommonStateGroupName = "CommonStates";
-
         private const string CollapsedStateName = "Collapsed";
-
+        private const string CommonStateGroupName = "CommonStates";
         private const string VisibleStateName = "Visible";
 
         public VGtimePagerHost()
@@ -36,11 +33,27 @@ namespace VGtime.Uwp.Controls
         protected override void OnApplyTemplate()
         {
             base.OnApplyTemplate();
+
+            UpdateVisualState(false);
         }
 
         private static void OnIsVisibleChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            throw new NotImplementedException();
+            var obj = (VGtimePagerHost)d;
+
+            obj.UpdateVisualState(true);
+        }
+
+        private void UpdateVisualState(bool useTransitions)
+        {
+            if (IsVisible)
+            {
+                VisualStateManager.GoToState(this, VisibleStateName, useTransitions);
+            }
+            else
+            {
+                VisualStateManager.GoToState(this, CollapsedStateName, useTransitions);
+            }
         }
     }
 }
