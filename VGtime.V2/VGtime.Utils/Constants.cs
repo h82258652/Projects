8ca8ae3627189962b3171ee01db40c2b31be363a
h82258652 <h82258652@hotmail.com>
2017-07-18 16:51:30 +0800

    添加 UserAgent 帮助类

--- VGtime.V2/VGtime.Utils/Constants.cs
+++ VGtime.V2/VGtime.Utils/Constants.cs
@@ -4,6 +4,8 @@
     {
         public const string AppVersionName = "2.0.8";
 
+        public const string DefaultUserAgent = "Xiaomi_2014216_TD-LTE/V1 Linux/3.4.0 Android/4.4.4 Release/20.10.2014 Browser/AppleWebKit537.36 Mobile Safari/537.36 System/Android 4.4.4 XiaoMi/MiuiBrowser/2.0.1";
+
         public const string PasswordEncryptKey = "VgTime!#@2548934";
 
         public const string SinaWeiboAppKey = "4154146479";
