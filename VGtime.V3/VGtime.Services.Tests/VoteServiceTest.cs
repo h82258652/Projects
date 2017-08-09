64972a4a702930e212477c1a453ae1b2917d2da8
h82258652 <h82258652@hotmail.com>
2017-08-09 17:25:25 +0800

    update

---
+++ VGtime.V3/VGtime.Services.Tests/VoteServiceTest.cs
@@ -0,0 +1,12 @@
+﻿namespace VGtime.Services.Tests
+{
+    public class VoteServiceTest
+    {
+        private readonly IVoteService _voteService;
+
+        public VoteServiceTest()
+        {
+            _voteService = new VoteService();
+        }
+    }
+}
