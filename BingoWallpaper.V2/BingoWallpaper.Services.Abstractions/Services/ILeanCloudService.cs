4aa7b3c34cd1b2f9fdc5be18b328e1f8cbcf7fe5
h82258652 <h82258652@hotmail.com>
2017-08-15 10:58:32 +0800

    update

---
+++ BingoWallpaper.V2/BingoWallpaper.Services.Abstractions/Services/ILeanCloudService.cs
@@ -0,0 +1,6 @@
+﻿namespace BingoWallpaper.Services
+{
+    public interface ILeanCloudService
+    {
+    }
+}
---
+++ BingoWallpaper.V2/BingoWallpaper.Services.Tests/LeanCloudServiceTest.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace BingoWallpaper.Services.Tests
+{
+    public class LeanCloudServiceTest
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
