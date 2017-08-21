8852f70fd013bd22bb69d80c6ca525b271cd0224
h82258652 <h82258652@hotmail.com>
2017-08-21 11:15:10 +0800

    update

--- BingoWallpaper.V2/BingoWallpaper.Core/Constants.cs
+++ BingoWallpaper.V2/BingoWallpaper.Core/Constants.cs
@@ -3,5 +3,9 @@
     public static class Constants
     {
         public const string BingUrlBase = "http://global.bing.com";
+
+        public const string LeanCloudAppId = "2odv0fmdni1w22hceawylo48l76vxbltgpl1mnoq3hlxj55j";
+
+        public const string LeanCloudAppKey = "idsoc6l9k218zrge2qi06anel3qcoqgvhutbqm93e4l58d3i";
     }
 }
