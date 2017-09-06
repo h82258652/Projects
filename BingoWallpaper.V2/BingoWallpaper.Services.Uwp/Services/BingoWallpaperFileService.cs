c5348e54fd0f22d4e9cc3377c837ed16d52cae68
h82258652 <h82258652@hotmail.com>
2017-09-06 13:04:00 +0800

    update

--- BingoWallpaper.V2/BingoWallpaper.Services.Uwp/Services/BingoWallpaperFileService.cs
+++ BingoWallpaper.V2/BingoWallpaper.Services.Uwp/Services/BingoWallpaperFileService.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Windows.Storage.Pickers;
 
 namespace BingoWallpaper.Services
 {
     public class BingoWallpaperFileService : IBingoWallpaperFileService
     {
-        public Task SaveFileAsync(string suggestedFileName, byte[] bytes)
+        public async Task SaveFileAsync(string suggestedFileName, byte[] bytes)
         {
+            if (bytes == null)
+            {
+                throw new ArgumentNullException(nameof(bytes));
+            }
+
+            var fileSavePicker = new FileSavePicker();
+
+            var file = await fileSavePicker.PickSaveFileAsync();
+
             throw new NotImplementedException();
         }
     }
